533de8723af6a4c4288c3f488e45b1263beafde7
BriannaLoveless <briannabel97@gmail.com>
2024-12-08 16:46:08 -0700

    final algorithm tweaks and polish

---
+++ README.md
@@ -0,0 +1,24 @@
+# Duplicate Detector
+Author: Brianna Loveless
+This application is used to detect duplicate advertiser names from a list of advertisers.
+
+## How to Run
+1. Install dependencies: `npm install`
+2. Ensure input file located in `input` folder is up to date
+3. Execute application: `node index.js`
+
+Note: Application may take several minutes to complete due to input size.
+
+## Context
+- The Sørensen–Dice algorithm is used as it provides effective similarity matching with adequate efficiency. 
+    - This was chosen in favor of a Levenshtein Distance algorithm for its increased accuracy given the provided data set.
+    - Limitations of the Sørensen–Dice algorithm include decreased accuracy for shorter strings due to the use of bigrams for the comparison.
+- Default similarity score threshold values were set based on testing and are configurable to accommodate other datasets.
+- Multithreading was left out to maintain simplicity in this implementation.
+
+## Potential Improvements
+1. Multithreading using Javascript Service Workers would reduce the overall time cost of the implementation.
+2. Preprocessing the comparison list with a cheaper algorithm may result in some time savings.
+3. Using a machine learning clustering approach may yield more accurate results and time savings on large datasets. This approach was not implemented due to assessment time constraints.
+4. Writing the output to a file could improve usability by other applications.
+5. Sorting the output by similarity score sould improve usefullness.
--- index.js
+++ index.js
@@ -1,29 +1,76 @@
-import { stringSimilarity } from "string-similarity-js";
+import { stringSimilarity } from 'string-similarity-js';
 import fs from 'node:fs';
 
-const data = fs.readFileSync('./input/advertisers.txt', 'utf8').split("\r\n");
-let output = {}
-// const test = ["42nd Street Moon", "42nd Street Moon LLC", "Publix"]
+const data = fs.readFileSync('./input/advertisers.txt', 'utf8').split('\r\n');
 
-function compare(value, array) {
-    output[value] = []
-    array.forEach((nextValue) => {
-      if (value == nextValue) {
-        return;
-      } else {
-        let score = {
-          advertiserName: nextValue,
-          score: stringSimilarity(value, nextValue)
+/**
+* Computes the string similarities between the provided 
+* advertiser and all known advertisers and returns the results. 
+* Results are filtered based on possible and likely thresholds.
+*/
+function computeSimilarities(currentAdvertiser, allAdvertisers, likelyThreshold=0.8, possibleThreshold=0.6) {
+    // 1. Compute similarity
+    let similarities = allAdvertisers.map((advertiser) => {
+        // Ignore current advertiser
+        if (currentAdvertiser == advertiser) {
+            return { name: advertiser, score: -1 };
         }
-        output[value].push(score)
+        const score = stringSimilarity(currentAdvertiser, advertiser)
+        return { name: advertiser, score: score };
+    });
+
+    // 2. Filter results
+    const likelyMatches = similarities.filter((similarity) => similarity.score >= likelyThreshold);
+    const possibleMatches = similarities.filter((similarity) => similarity.score >= possibleThreshold && similarity.score < likelyThreshold);
+
+    // 3. Format Output
+    const filteredResults = { likelyMatches, possibleMatches };
+    return filteredResults
+}
+
+/**
+ * Outputs the results of duplicate detection to the console.
+ */
+function printResults(similarityResults) {
+    console.log()
+    for (const companyName in similarityResults) {
+        const likelyMatches = similarityResults[companyName].likelyMatches.map((m) => m.name);
+        const possibleMatches = similarityResults[companyName].possibleMatches.map((m) => m.name);
+
+        if(likelyMatches.length > 0 || possibleMatches.length > 0) { 
+          console.log(companyName); 
+        if (likelyMatches.length > 0) {
+            console.log(`    Likely Duplicate: ${likelyMatches.join(', ')}`);
+        }
+        if (possibleMatches.length > 0) {
+            console.log(`    Possible Duplicate: ${possibleMatches.join(', ')}`);
+        }
+        console.log();
       }
-    })
+    }
+}
+
+/** Program Entrypoint */
+function main() {
+  console.log("Note: This may take several minutes to run based on input size")
+  console.log()
+  // 1. Compute Similarities for all advertisers
+  const similarityResults = {}
+  let computationProgress = -1
+  for (const [index, advertiser] of data.entries()) {
+    similarityResults[advertiser] = computeSimilarities(advertiser, data)
+
+    // Output Progress
+    const currentProgress = parseInt((index / data.length) * 100)
+    if(currentProgress % 1 == 0 && currentProgress != computationProgress){
+      process.stdout.cursorTo(0);
+      process.stdout.write(`Progress: ${currentProgress}%`);
+      computationProgress = currentProgress
+    }
   }
 
-function outputMatches() {
-  
+  // 2. Output Results
+  printResults(similarityResults);
 }
-  
-data.forEach((v, idx) => compare(v, data))
-// test.forEach((v, idx) => compare(v, test))
-console.log(output)
+
+main()
